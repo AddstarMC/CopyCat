1084c19f2805ebfa7d9983006824428d30cac12e
Narimm <benjicharlton@gmail.com>
2018-12-20 09:43:25 +1000

    Fix 1.13 build

--- src/main/java/au/com/addstar/copycat/logic/EliminationMainState.java
+++ src/main/java/au/com/addstar/copycat/logic/EliminationMainState.java
@@ -3,10 +3,6 @@ package au.com.addstar.copycat.logic;
 import java.util.HashSet;
 
 import au.com.mineauz.minigames.MinigameMessageType;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
-import org.bukkit.boss.BarColor;
-import org.bukkit.boss.BossBar;
 
 import au.com.mineauz.minigames.MinigamePlayer;
 import au.com.mineauz.minigames.Minigames;
@@ -14,6 +10,10 @@ import au.com.mineauz.minigames.minigame.Minigame;
 
 import au.com.addstar.copycat.GameBoard;
 import au.com.addstar.copycat.PlayerStation;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BossBar;
 
 public class EliminationMainState extends MainState
 {
@@ -43,7 +43,7 @@ public class EliminationMainState extends MainState
 
 			player.sendMessage("You did not finish in time. You have lost a life", MinigameMessageType.ERROR);
 			game.broadcast(player.getDisplayName() + " lost a life.", player);
-			player.getPlayer().getWorld().playSound(player.getPlayer().getLocation(), Sound.ENTITY_IRONGOLEM_HURT, 1f, 10);
+			player.getPlayer().playSound(player.getPlayer().getLocation(), Sound.ENTITY_IRON_GOLEM_HURT, 1f, 10);
 			player.getPlayer().getWorld().spawnParticle(Particle.VILLAGER_ANGRY, player.getPlayer().getLocation(), 10, 1, 1, 1, 0);
 			
 			if(player.getDeaths() >= minigame.getLives())
